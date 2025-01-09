cb17515175a8ce3455f196be6251d6ee02f19253
taesik <xmflr28@gmail.com>
2025-01-09 18:22:03 +0900

    test11

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ const app = express()
 
 app.get('/server1', (req,res)=> {
     const header = req.headers
-    const secret = process.env.ENV_SECRET
     console.log("Get : Server1")
-    res.status(200).json({header , secret});
+    res.status(200).json({header , message : new Date()});
 })
 
 app.listen(7777, () => {
