1ebfd3901d02c135de035095c3c045237cb1fabc
taesik <xmflr28@gmail.com>
2025-01-09 18:22:59 +0900

    test11

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/server1', (req,res)=> {
     const header = req.headers
     console.log("Get : Server1")
-    res.status(200).json({header , message : new Date()});
+    res.status(200).json({header , date : new Date()});
 })
 
 app.listen(7777, () => {
